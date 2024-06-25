132e13c38b14cebbede36244b657d679b3b4786f
Jules <jules.gresset@epitech.eu>
2024-06-25 13:23:59 +0200

    fix: console.log dans le fichier de config retiré

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-console.log(process.env.DB_URL);
 const client = new MongoClient(process.env.DB_URL, { serverSelectionTimeoutMS: 500 });
 const DB_NAME = process.env.DB_NAME;
 
