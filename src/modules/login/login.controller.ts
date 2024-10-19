c6ec699d8ab7c447b09a47a0c4c96ba8b66ceb44
Xantass <theo.fouque@epitech.eu>
2024-10-19 00:49:40 -0400

    fix: change params request of the route 'api/login' to three query params 'password, username, idRestaurant'

--- src/modules/login/login.controller.ts
+++ src/modules/login/login.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Req, Get, BadRequestException } from '@nestjs/common';
+import { Controller, Req, Get, BadRequestException, Query } from '@nestjs/common';
 import { LoginService } from './login.service';
 
 // Controller for handling login requests
@@ -10,18 +10,22 @@ export class LoginController {
   /**
    * Authenticates a user based on the provided credentials.
    * 
-   * @param {Request} request - The login credentials containing username, password and idRestaurant.
+   * @param {string} password - The password of the user.
+   * @param {string} username - The username of the user.
+   * @param {number} idRestaurant - The id of the restaurant where containing the user.
    * @returns {Promise<any>} The authentication result.
    * @throws {UnauthorizedException} If authentication fails.
    */
   @Get()
-  async login(@Req() request: Request) {
+  async login(@Query('password') password: string,
+              @Query('username') username: string,
+              @Query('idRestaurant') idRestaurant: number) {
     try {
       // Attempt to authenticate the user using the service
       const auth = await this.loginService.authenticateUser(
-        Number(request.body['idRestaurant']),
-        request.body['username'],
-        request.body['password'],
+        Number(idRestaurant),
+        username,
+        password,
       );
       const token = await this.loginService.login(auth);
       return token; // Return the authentication result
