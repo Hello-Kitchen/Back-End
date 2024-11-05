7fd405fa162d8add8d731618be17ea580b3a08c2
Xantass <theo.fouque@epitech.eu>
2024-11-04 19:38:45 -0500

    fix: route get with id and wihtout return 400 if you don't use all params, now return nothing and continue into controller

--- src/modules/orders/pipe/status.pipe.ts
+++ src/modules/orders/pipe/status.pipe.ts
@@ -3,6 +3,7 @@ import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
 @Injectable()
 export class StatusPipe implements PipeTransform {
   transform(value: any) {
+    if (value === undefined) return value
     if (typeof value !== 'string') throw new BadRequestException();
     if (value !== 'pending' && value !== 'ready')
       throw new BadRequestException();
