68e9b5d957b830d95ced70719256656a76c4ea2b
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-11-26 09:05:21 +0100

    fix: remove linter errors

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,9 +1,4 @@
-import {
-    Controller,
-    Get,
-    HttpStatus,
-    UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../../shared/guards/jwt-auth.guard';
 
 /**
@@ -15,24 +10,23 @@ import { JwtAuthGuard } from '../../shared/guards/jwt-auth.guard';
  */
 @Controller('api/health')
 export class HealthController {
-
-    /**
-     * Performs a health check on the server.
-     *
-     * This method verifies the health of the server and returns a simple
-     * response indicating whether the server is running correctly.
-     * It requires JWT authentication to access.
-     *
-     * @returns {Object} - An object containing the HTTP status and a health message.
-     * @throws {UnauthorizedException} - Throws if the request is not authenticated.
-     * @async
-     */
-    @UseGuards(JwtAuthGuard)
-    @Get()
-    async checkHealth() {
-        return {
-            status: HttpStatus.OK,
-            message: 'Server is healthy',
-        };
-    }
+  /**
+   * Performs a health check on the server.
+   *
+   * This method verifies the health of the server and returns a simple
+   * response indicating whether the server is running correctly.
+   * It requires JWT authentication to access.
+   *
+   * @returns {Object} - An object containing the HTTP status and a health message.
+   * @throws {UnauthorizedException} - Throws if the request is not authenticated.
+   * @async
+   */
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async checkHealth() {
+    return {
+      status: HttpStatus.OK,
+      message: 'Server is healthy',
+    };
+  }
 }
